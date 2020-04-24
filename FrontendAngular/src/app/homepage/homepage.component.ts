8e5e1a19ecd41dd82363e8affe015aab81ccdeb9
Gabriel Cabral <ccabral@prounlimited.com>
2020-04-24 00:46:27 -0700

    finish setting up form group

--- FrontendAngular/src/app/homepage/homepage.component.ts
+++ FrontendAngular/src/app/homepage/homepage.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { FormGroup, FormControl, Validators } from '@angular/forms';
+import { FormGroup, FormControl, Validators, FormBuilder, FormGroupName } from '@angular/forms';
 
 @Component({
   selector: 'app-homepage',
@@ -8,27 +8,64 @@ import { FormGroup, FormControl, Validators } from '@angular/forms';
 })
 export class HomepageComponent implements OnInit {
 
-  message = "";
-  userInfoForm = new FormGroup({
-    currentAge: new FormControl(25, [Validators.required]),
-    retirementAge: new FormControl(60, [Validators.required]),
-    deathAge: new FormControl(60, [Validators.required]),
-    // email: new FormControl('', [Validators.required, Validators.email]),
-    // body: new FormControl('', Validators.required)
-  });
+  retirementForm: FormGroup;
   submitted = false;
+  total = 0;
 
-  constructor() { }
+  constructor(private fb: FormBuilder) { }
 
-  ngOnInit(): void {
-    this.message = "first message";
+  ngOnInit() {
+    this.initForms();
+    this.initSubscribers();
+  }
+
+  initForms() {
+    this.retirementForm = this.fb.group({
+      currentAge: [25, [Validators.required]],
+      retirementAge: [60, [Validators.required]],
+      deathAge: [90, [Validators.required]],
+      expenses: this.fb.group({
+        homeCosts: [615, [Validators.required]],
+        carCosts: [220, [Validators.required]],
+        healthcare: [600, [Validators.required]],
+        food: [600, [Validators.required]],
+        vacation: [250, [Validators.required]],
+        personal: [500, [Validators.required]],
+      })
+    });
+    const expenses = this.retirementForm.controls.expenses as FormGroup;
+    this.total = Object.keys(expenses.controls).reduce((acc, cur) => expenses.get(cur).value + acc, 0);
+  }
+
+  initSubscribers() {
+    this.retirementForm.valueChanges.subscribe(val => {
+      const expenses: any = val.expenses;
+      this.total = Object.keys(expenses).reduce((acc, cur) => expenses[cur] + acc, 0);
+    });
   }
 
   submitUserInfo() {
+    console.log("submitting");
     this.submitted = true;
   }
 
-  get uif() {
-    return this.userInfoForm.controls;
+  formatMoney(ctrlName: string) {
+    const ctrl: FormControl = this.retirementForm.controls[ctrlName] as FormControl;
+    let value = ctrl.value;
+    value = value.replace(/\B(?=(\d{3})+(?!\d))/g, ",");
+    value = '$'+value;
+    ctrl.setValue(value);
+  }
+
+  unformatMoney(ctrlName: string) {
+    const ctrl: FormControl = this.retirementForm.controls[ctrlName] as FormControl;
+    let value = ctrl.value;
+    value = value.replace(new RegExp("\\$", "g"), ""); // two different ways to do this
+    value = value.split(",").join("");
+    ctrl.setValue(value);
+  }
+
+  get rf() {
+    return this.retirementForm.controls;
   }
 }
